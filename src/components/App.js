1248d8c0c4254c7ea65ce6c9b7d6194f7218479b
adendaiy <adenabdib@gmail.com>
2024-07-17 22:47:33 +0100

    this is my react fetch lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,58 @@
-// create your App component here
+// import React, { useEffect, useState } from 'react'
+
+// const App = () => {
+
+// const[dogImages, setDogImages]= useState([]);
+// const [isLoaded, setIsLoaded] = useState(false);
+
+// useEffect(()=>{
+//     fetch("https://dog.ceo/api/breeds/image/random")
+//     .then((response) => response.json())
+//     .then((data)=>{
+//         setDogImages(data.message)
+//         setIsLoaded(true);
+//     })
+// },[])
+//     if (!isLoaded) return <p>Loading...</p>; 
+//     return (
+//         <div>
+            
+//             <image> src= {dogImages} alt= "A Random Dog"</image>
+    
+//         </div>
+//     )
+// }
+
+// export default App;
+
+
+
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogImage, setDogImage] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then(response => response.json())
+      .then(data => {
+        setDogImage(data.message);
+        setIsLoading(false); 
+      });
+  }, []);
+  
+
+  return (
+    <div className="App">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
+
